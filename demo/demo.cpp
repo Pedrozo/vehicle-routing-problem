2192fc20ced4bb6dd16b20ae7a032f88c64440d6
Leonardo Pedrozo Amaral <leonardo.pedrozo@hotmail.com>
2021-10-10 14:08:00 -0300

    directory structure, dummy demo and simple CMake

---
+++ demo/demo.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "vrp/foo.hpp"
+
+int main() {
+	std::cout << do_foo() << std::endl;
+
+	return 0;
+}
---
+++ src/vrp/foo.cpp
@@ -0,0 +1,5 @@
+#include "vrp/foo.hpp"
+
+int do_foo() {
+	return 666;
+}
